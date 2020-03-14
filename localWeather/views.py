36cac58c5a026058f762d937c6ee7dbd88e1332c
Ajeet Singh <ajeetsingh1646@gmail.com>
2020-03-14 22:31:19 +0530

    Jumbotron with features

--- localWeather/views.py
+++ localWeather/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html',{})
+    import json
+    import requests
+    api_request = requests.get("https://api.waqi.info/feed/here/?token=f6320186d06d4bc2ed866ab0fe51bfd6163b72f4")
+    try:
+        api = json.loads(api_request.content)
+        #a = api.data.aqi
+        #status = ""
+    except Exception as e:
+        api = "Error..."
+    
+    return render(request,'index.html',{"api":api,
+                                       "aq":api["data"]["aqi"]})
+
 
 def about(request):
     return render(request, 'about.html',{})
